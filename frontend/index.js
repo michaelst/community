f14bf58344ffb85f5805fecf0d57209850650292
Michael St Clair <michaelst57@gmail.com>
2021-07-27 21:46:06 -0600

    reset

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import appInfo from './app.json';
-
-AppRegistry.registerComponent(appInfo.name, () => App);
